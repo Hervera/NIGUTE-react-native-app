b6b11a6df7e2a60258fb2eb32062e26f370ce918
Herve Nkurikiyimfura <hervenkurikiyimfura@Herves-MacBook-Pro.local>
2020-03-02 21:06:39 +0200

    feature(UI): design subscribe screen
    - add a form with email input
    - add a subscrib button

--- src/app/components/Subscribe.js
+++ src/app/components/Subscribe.js
@@ -1,24 +1,46 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {View, StatusBar, KeyboardAvoidingView} from 'react-native';
+import {Block, Text, Input, Button} from 'galio-framework';
+import {niguteTheme} from '../constants';
+import {ScrollView} from 'react-native-gesture-handler';
+import styles from '../styles/index.style';
 
 export default class Subscribe extends Component {
-  //Subscribe Component
-  static navigationOptions = {
-    drawerLabel: 'Subscribe',
-  };
   render() {
     return (
-      <View style={styles.MainContainer}>
-        <Text style={{fontSize: 23}}> Subscribe </Text>
-      </View>
+      <ScrollView middle>
+        <StatusBar hidden />
+        <View style={styles.container}>
+          <Block middle>
+            <Block style={[styles.formContainer, {marginTop: 20}]}>
+              <Block middle style={styles.heading}>
+                <Text size={16} style={{padding: 20}}>
+                  Subscribe to Stay Updated
+                </Text>
+              </Block>
+              <Block style={styles.formBlock}>
+                <Block center>
+                  <KeyboardAvoidingView behavior="padding" enabled>
+                    <Block style={styles.blockInput}>
+                      <Input
+                        placeholder="Enter your Email*"
+                        style={styles.input}
+                      />
+                    </Block>
+                    <Block middle>
+                      <Button color="primary" style={styles.sendButton}>
+                        <Text bold size={14} color={niguteTheme.COLORS.WHITE}>
+                          Subscribe
+                        </Text>
+                      </Button>
+                    </Block>
+                  </KeyboardAvoidingView>
+                </Block>
+              </Block>
+            </Block>
+          </Block>
+        </View>
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  MainContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
